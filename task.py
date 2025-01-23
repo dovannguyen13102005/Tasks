281f0e263fba43e8a71f97c8641524ecfb50504b
dovannguyen13102005 <dovannguyen13102005@gmail.com>
2025-01-23 23:21:50 +0700

    new task

--- task.py
+++ task.py
@@ -1,2 +1,41 @@
-print("Hi cat")
-print("Hi fish")
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def get_in4(self):       #Abstraction
+        pass
+    @abstractmethod
+    def talk(self):          #Abstraction
+        pass
+
+#Encapsulation
+class Dog(Animal):
+    def __init__(self, name):
+        self.__name = name
+    def get_in4(self):
+        return f"Name of dog is: {self.__name}"
+    def talk(self):
+        return f"{self.__name} keu gau gau"
+
+#Encapsulation
+class Cat(Animal):
+    def __init__(self, name):
+        self.__name = name
+    def get_in4(self):
+        return f"Name of cat is: {self.__name}"
+    def talk(self):
+        return f"{self.__name} keu meo meo"
+#Inheritance
+class Tabby(Cat):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.__age = age
+    def get_in4(self):
+        return (f"{super().get_in4()}\n"
+                f"Age: {self.__age}")
+
+animals = [Dog("Trung"), Cat("Nam"), Tabby("Hoang", 8)]
+#Polymorphism
+for animal in animals:
+    print("\n" + animal.get_in4())
+    print(animal.talk())
